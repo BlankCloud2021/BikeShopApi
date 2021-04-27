73bfd638340b4e26e9b13855c0b67afd546987e8
Kent Moseley <moseleykent856@gmail.com>
2021-04-27 09:15:16 -0400

    Crud Created For Models

---
+++ BikeShopApi/Controllers/BikeShopsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BikeShopApi.Models;
+
+namespace BikeShopApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BikeShopsController : ControllerBase
+    {
+        private readonly BikeShopApiDbContext _context;
+
+        public BikeShopsController(BikeShopApiDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BikeShops
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BikeShop>>> GetBikeShops()
+        {
+            return await _context.BikeShops.ToListAsync();
+        }
+
+        // GET: api/BikeShops/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BikeShop>> GetBikeShop(int id)
+        {
+            var bikeShop = await _context.BikeShops.FindAsync(id);
+
+            if (bikeShop == null)
+            {
+                return NotFound();
+            }
+
+            return bikeShop;
+        }
+
+        // PUT: api/BikeShops/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBikeShop(int id, BikeShop bikeShop)
+        {
+            if (id != bikeShop.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bikeShop).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BikeShopExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BikeShops
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<BikeShop>> PostBikeShop(BikeShop bikeShop)
+        {
+            _context.BikeShops.Add(bikeShop);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBikeShop", new { id = bikeShop.Id }, bikeShop);
+        }
+
+        // DELETE: api/BikeShops/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBikeShop(int id)
+        {
+            var bikeShop = await _context.BikeShops.FindAsync(id);
+            if (bikeShop == null)
+            {
+                return NotFound();
+            }
+
+            _context.BikeShops.Remove(bikeShop);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BikeShopExists(int id)
+        {
+            return _context.BikeShops.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ BikeShopApi/Controllers/ExtraFeaturesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BikeShopApi.Models;
+
+namespace BikeShopApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExtraFeaturesController : ControllerBase
+    {
+        private readonly BikeShopApiDbContext _context;
+
+        public ExtraFeaturesController(BikeShopApiDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ExtraFeatures
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ExtraFeature>>> GetExtraFeatures()
+        {
+            return await _context.ExtraFeatures.ToListAsync();
+        }
+
+        // GET: api/ExtraFeatures/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ExtraFeature>> GetExtraFeature(int id)
+        {
+            var extraFeature = await _context.ExtraFeatures.FindAsync(id);
+
+            if (extraFeature == null)
+            {
+                return NotFound();
+            }
+
+            return extraFeature;
+        }
+
+        // PUT: api/ExtraFeatures/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutExtraFeature(int id, ExtraFeature extraFeature)
+        {
+            if (id != extraFeature.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(extraFeature).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExtraFeatureExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ExtraFeatures
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ExtraFeature>> PostExtraFeature(ExtraFeature extraFeature)
+        {
+            _context.ExtraFeatures.Add(extraFeature);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetExtraFeature", new { id = extraFeature.Id }, extraFeature);
+        }
+
+        // DELETE: api/ExtraFeatures/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExtraFeature(int id)
+        {
+            var extraFeature = await _context.ExtraFeatures.FindAsync(id);
+            if (extraFeature == null)
+            {
+                return NotFound();
+            }
+
+            _context.ExtraFeatures.Remove(extraFeature);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ExtraFeatureExists(int id)
+        {
+            return _context.ExtraFeatures.Any(e => e.Id == id);
+        }
+    }
+}
